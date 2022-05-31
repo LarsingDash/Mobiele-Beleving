c654b68eb455663455d6f4d2e0ad1ac07023a48a
LarsingDash <l.villevoye@student.avans.nl>
2022-05-31 16:18:16 +0200

    Start activity branch

--- app/src/main/java/com/example/mobielebeleving/Activities/MainActivity.java
+++ app/src/main/java/com/example/mobielebeleving/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.mobielebeleving;
+package com.example.mobielebeleving.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.mobielebeleving.R;
 
 public class MainActivity extends AppCompatActivity {
 
