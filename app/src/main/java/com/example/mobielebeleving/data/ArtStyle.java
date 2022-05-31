39583910e4d134fbd0646ba726aa79e60f43ff99
Michelle <mj.vandesande1@student.avans.nl>
2022-05-31 16:11:31 +0200

    ArtStyle Class made

---
+++ app/src/main/java/com/example/mobielebeleving/data/ArtStyle.java
@@ -0,0 +1,25 @@
+package com.example.mobielebeleving.data;
+
+import android.graphics.Color;
+import android.media.Image;
+
+public class ArtStyle {
+    Image image;
+    Color color;
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
